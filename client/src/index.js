187f0e0d720a3a91b33281862a51cbf0388f7a61
satya596 <satyanarayana.p@motivitylabs.com>
2023-10-25 15:02:02 +0530

    UI_serch

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,8 @@ import reportWebVitals from './reportWebVitals';
 import './index.css';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-
 import { configureStore } from "./store";
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
