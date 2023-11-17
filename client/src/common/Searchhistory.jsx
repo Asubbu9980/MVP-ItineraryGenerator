8cf2d8f2d10db8d28641b9a301e84048dbf5fec7
satya596 <satyanarayana.p@motivitylabs.com>
2023-11-17 17:17:50 +0530

    Search History scrolling
    Search History scrolling

--- client/src/common/Searchhistory.jsx
+++ client/src/common/Searchhistory.jsx
@@ -4,16 +4,44 @@ import Chip from '@mui/material/Chip';
 import { Box } from '@mui/material';
 
 function Searchhistory() {
+    const chipData = [
+        {
+          "id": 1,
+          "label": "Hyderabad to Vizag"
+        },
+        {
+          "id": 2,
+          "label": "Hyderabad to Goa"
+        },
+        {
+          "id": 3,
+          "label": "Banglore to Delhi"
+        },
+        {
+            "id": 4,
+            "label": "Hyderabad to Vizag"
+          },
+          {
+            "id": 5,
+            "label": "Hyderabad to Goa"
+          },
+          {
+            "id": 6,
+            "label": "Banglore to Delhi"
+          }
+      ];
+    
     return (
         <div className='container p-0'>
             <Box className='searchHistory' >
                 <h6 style={{ color: '#fff', fontWeight: 'bold' }}>Recent Search</h6>
-                <div style={{ display: 'flex', gap: 10 }}>
-                    <Chip label="Hyderabad to Vizag" variant="outlined" sx={{ borderColor: '#FFF', color: '#FFF', height: '25px', fontSize: '12px' }} />
-                    <Chip label="Hyderabad to Goa " variant="outlined" sx={{ borderColor: '#FFF', color: '#FFF', height: '25px', fontSize: '12px' }} />
-                    <Chip label="Banglore to Delhi" variant="outlined" sx={{ borderColor: '#FFF', color: '#FFF', height: '25px', fontSize: '12px' }} />
-                </div>
-            </Box>
+                <div className='moveing' >
+                 {chipData.map(item => (
+                    <Chip key={item.id} label={item.label} variant="outlined" sx={{ borderColor: '#FFF', color: '#FFF', height: '25px', fontSize: '12px' }} />
+                  ))}
+
+</div>
+             </Box>
 
         
         </div>
