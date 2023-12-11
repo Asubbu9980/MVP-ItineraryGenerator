32434303911c2ac961c7fa50998fa45570c0fabc
Subbu-digitalReef <subrahmanyam.annavaram@digitalreef.com>
2023-12-11 15:53:54 +0530

    Redis Implemented

---
+++ server/src/services/common/cacheHandler.js
@@ -0,0 +1,47 @@
+const redis = require('redis');
+let cluster;
+const redisPort = 6397;
+const redisHost = '127.0.0.1';
+exports.initializeRedis = async () => {
+    const LOCAL_REDIS = process.env.NODE_ENV == 'production' ? false : true;
+    cluster = redis.createClient(redisPort, redisHost);
+    cluster.on('error', (err) => console.log('Redis Cluster Error', err));
+    console.log('connected to cluster...');
+    await cluster.connect();
+    cluster.set("test-value", "test");
+    cluster.get("test-value").then(r => {
+        console.log(r); // <- you should see "test"
+    });
+}
+
+exports.setCache = (key, value) => {
+    const prefix = process.env.NODE_ENV == 'production' ? '-prod-' : '-dev-';
+    const finalKey = prefix + '-' + key;
+    cluster.set(finalKey, value, 'ex', 525948766);
+}
+
+exports.setDefaultCache = (key, value) => {
+    const prefix = process.env.NODE_ENV == 'production' ? '-prod-' : '-dev-';
+    const finalKey = prefix + '-' + key;
+    cluster.set(finalKey, value, 'ex', 3600);
+}
+
+exports.setHashCache = (hashKey, key, value) => {
+    cluster.hset(hashKey, key, value)
+}
+exports.getHashCache = (hashKey, key) => {
+    return cluster.hget(hashKey, key);
+}
+
+exports.getCache = (key) => {
+    const prefix = process.env.NODE_ENV == 'production' ? '-prod-' : '-dev-';
+    const finalKey = prefix + '-' + key;
+    return cluster.get(finalKey);
+}
+exports.deleteKey = (key) => {
+    return cluster.del(key)
+}
+
+exports.getHashKeys = (hashKey) => {
+    return cluster.hkeys(hashKey);
+}
