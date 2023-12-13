4f645626834535c578308dc9bdf4db5d3b3baad8
Subbu-digitalReef <subrahmanyam.annavaram@digitalreef.com>
2023-12-13 16:52:52 +0530

    Update to gpt 4

--- server/src/openai.js
+++ server/src/openai.js
@@ -1,4 +1,4 @@
-const { Configuration, OpenAIApi } = require("openai");
+// const { Configuration, OpenAIApi } = require("openai");
 
 const openaiApiKey = process.env.OPENAI_API_KEY;
 
@@ -7,20 +7,20 @@ if (!openaiApiKey) {
     process.exit(1);
 }
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
-module.exports = {
-    openai,
-};
-// const OpenAI = require('openai');
-
-// const openai = new OpenAI({
-//     apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+// const configuration = new Configuration({
+//     apiKey: process.env.OPENAI_API_KEY,
 // });
+// const openai = new OpenAIApi(configuration);
 
 // module.exports = {
 //     openai,
 // };
+const OpenAI = require('openai');
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+});
+
+module.exports = {
+    openai,
+};
